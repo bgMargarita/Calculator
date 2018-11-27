d6292b2b4405f50d57eb7195f02dd9bf6879ef1f
bgMargarita <margolyagavrilman@gmail.com>
2018-11-27 23:06:24 +0300

    reading from file(file not found )

--- src/main/java/com/Margarita/calculator/Parser.java
+++ src/main/java/com/Margarita/calculator/Parser.java
@@ -1,35 +1,37 @@
 package com.Margarita.calculator;
 
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
-
-import static com.sun.javaws.jnl.XMLFormat.process;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Created by Batman on 27.11.2018.
  */
 public class Parser {
     String filename = "file.txt";
-   try {
-        File file = new File("/Users/prologistic/file.txt");
-        //создаем объект FileReader для объекта File
-        FileReader fr = new FileReader(file);
-        //создаем BufferedReader с существующего FileReader для построчного считывания
-        BufferedReader reader = new BufferedReader(fr);
-        // считаем сначала первую строку
-        String line = reader.readLine();
-        while (line != null) {
-            System.out.println(line);
-            // считываем остальные строки в цикле
-            line = reader.readLine();
+
+    String line;
+    List<String> mass = new ArrayList<String>();
+
+    public void reader() {
+        BufferedReader readFromFile = null;
+        try {
+            readFromFile = new BufferedReader(new FileReader("C:\\Users\\Batman\\Downloads\\text.txt"));
+            while ((line = readFromFile.readLine()) != null) {
+            mass.add(readFromFile.readLine());
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (readFromFile != null)
+                try {
+                    readFromFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
         }
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
+
     }
-}
+
 }
