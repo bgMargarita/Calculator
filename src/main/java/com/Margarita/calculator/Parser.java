0392e5dd9f6ecc01a4ad27fc0c519afb9c6a8448
bgMargarita <margolyagavrilman@gmail.com>
2018-11-28 18:37:18 +0300

    remove String filename made ReadFromFile return List remove file.close() start postfix translation(need to do priority choice)

--- src/main/java/com/Margarita/calculator/Parser.java
+++ src/main/java/com/Margarita/calculator/Parser.java
@@ -1,40 +1,65 @@
 package com.Margarita.calculator;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by Batman on 27.11.2018.
  */
 public class Parser {
-    String line;
-    List<String> Array = new ArrayList<String>();
+    private static final Map<String, Integer> OPERATION_PRIORITY = new HashMap<String, Integer>();
 
-    public void readFromFile() {
+    static {
+        OPERATION_PRIORITY.put("*", 1);
+        OPERATION_PRIORITY.put("/", 1);
+        OPERATION_PRIORITY.put("+", 2);
+        OPERATION_PRIORITY.put("-", 2);
+    }
+
+    String line;
+
+    public List<String> StringToArray(String str) {
+        return Arrays.asList(str.split(" "));
+
+    }
+
+    public void WriteToStack(List<String> str) {
+        Stack stack = new Stack();
+        List<String> postfixString = new ArrayList<String>();
+        for (int i = 0; i < str.size(); i++) {
+            if (str.get(i).matches("[-+]?\\d+"))
+                stack.push(str.get(i));
+            else if (str.get(i) == "(")
+                stack.push(str.get(i));
+            else if (str.get(i) == ")") {
+                stack.push(str.get(i));
+                while (stack.lastElement() != "(") {
+                    postfixString.add(stack.pop().toString());
+                }
+                stack.pop();
+            }
+           /* else{ while(!stack.isEmpty() && stack.lastElement())
+
+            } выбор приоритетной функции
+*/
+        }
+    }
+
+    public List<String> ReadFromFile(String filename) {
+        List<String> stringsFromFile = new ArrayList<String>();
         BufferedReader readFromFile = null;
         try {
-            readFromFile = new BufferedReader(new FileReader("\\Users\\Batman\\IdeaProjects\\Calculator\\src\\file.txt"));
+            readFromFile = new BufferedReader(new FileReader(filename));
             while ((line = readFromFile.readLine()) != null) {
-                Array.add(line);
-                System.out.println("gdfg");
+                stringsFromFile.add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (readFromFile != null)
-                try {
-                    readFromFile.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
         }
-
+        return StringToArray(stringsFromFile.get(stringsFromFile.size() - 1));
     }
 
-    public static void main(String args[]) {
-        Parser parser = new Parser();
-
-    }
 
 }
